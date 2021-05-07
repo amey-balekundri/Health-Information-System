77099a8e96b32f09811f5b30269b5b4eb30697f8
Hemanshu Bafna <49424746+hbafna1@users.noreply.github.com>
2021-05-07 10:40:15 +0530

    Minor UI Changes
    * Minor UI Changes
    
    * Update patient_reports.html
    
    * Changes Done.

--- web/patient/admin.py
+++ web/patient/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from patient.models import Patient
+from patient.models import Patient,Basic
 admin.site.register(Patient)
+admin.site.register(Basic)
